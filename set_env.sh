d8e9a4ee29df532546e50df59d56295914e49784
Nicolas DE FORTIS <ndf.df@laposte.net>
2021-08-13 05:24:40 +0200

    Modification du Vagrantfile ; set_env.sh | Ajout de wsgi.py

--- set_env.sh
+++ set_env.sh
@@ -4,22 +4,25 @@ echo "Installation de python"
 sudo apt update
 sudo apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 
+echo "Téléchargement des fichiers github"
+git clone https://github.com/nicotrorigolo/tp_fil_rouge
+
 echo "Mise en place d'un environnement virtuel"
 sudo apt -y install python3-venv
 mkdir ~/myproject
 cd ~/myproject
+mv /home/vagrant/tp_fil_rouge/debut.py .
+mv /home/vagrant/tp_fil_rouge/wsgi.py
 python3 -m venv myprojectenv
 source myprojectenv/bin/activate
 
 echo "Configuration application Flask"
 pip install wheel
 pip install gunicorn flask
-echo "from flask import Flask
-> app = Flask(__name__)
-> 
-> @app.route("/")
-> def hello():
->     return "<h1 style='color:blue'>Hello There!</h1>"
-> 
-> if __name__ == "__main__":
->     app.run(host='0.0.0.0')" >toto.py
+sudo ufw allow 5000 # Indication au pare-feu que l'acces au port 5000 est autorise
+
+#echo "Verification que l'application fonctionne"
+#curl 192.168.100.50:5000 # 192.168.100.50 c'est l'adresse IP de la VM master, 5000 c'est le port de l'application
+
+echo "Configuration Gunicorn"
+gunicorn --bind 0.0.0.0:5000 wsgi:app
